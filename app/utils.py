5560b471e1c007ad45d544b2220550903e1779f7
Xngst <tarcvzf@protonmail.ch>
2024-02-07 21:38:52 +0100

    add Markup escape

--- app/utils.py
+++ app/utils.py
@@ -31,7 +31,8 @@ logger = logging.getLogger(__name__)
 
 THUMBNAIL_SIZE = (1000, 1000)
 FULL_SIZE = (1200, 2400)
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+
 
 def valid_email(email: str) -> bool:
     """
@@ -44,7 +45,7 @@ def valid_email(email: str) -> bool:
 
 
 def valid_image(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 def get_date():
